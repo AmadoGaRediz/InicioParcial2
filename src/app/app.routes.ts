e1d2c0b0d42177d2a412bd4c16a95f53b75b67e2
AmadoGaRediz <amadogr.ti22@utsjr.edu.mx>
2025-03-11 22:23:17 -0600

    subiendo inicio parcial2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'about',
+        component:AboutComponent
+    },
+    {
+        path:'inicio',
+        component:InicioComponent
+    },
+    {
+        path:'**',
+        redirectTo:'inicio'
+    }
+    
+
+    
+];
